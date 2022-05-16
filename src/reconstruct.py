149d40ee85e8d61f610aea312fd3e5a1eecbeaa0
Daniel Kaiser <dkaiser4897@gmail.com>
2022-05-16 19:30:04 -0400

    Update src/reconstruct.py; Add boilerplate, skeleton

--- src/reconstruct.py
+++ src/reconstruct.py
@@ -4,6 +4,7 @@
 # ===================== IMPORTS =======================
 # Standard library
 import random
+from itertools import chain
 
 # Scientific computing
 import numpy as np
@@ -39,112 +40,115 @@ def process_edge_classifications(classifications):
 
 # ---------- Calculation helpers -----------
 def calculate_edge_likelihoods(edge, degrees, mu=0.5, partitions=None):
-    pass
+    # Book-keeping
+    src = edge[0]
+    tgt = edge[1]
+
+    degree_products = {
+        layer_idx: degrees[layer_idx][src]*degrees[layer_idx][tgt]
+        for layer_idx in degrees
+        }
+
+    adj_mu = \
+        lambda src_partition, tgt_partition, mu: mu if src_partition == tgt_partition else 1-mu
+    denominator = sum(degree_products.values())
+    likelihoods = {
+        layer_idx: \
+            adj_mu(partitions[layer_idx][src], partitions[layer_idx][tgt], mu) \
+            * (degree_products[layer_idx] / denominator)
+        for layer_idx in degree_products
+    }
+
+    return likelihoods
 
 def get_partitions(remnants):
-    pass
+    # Calculate partitions of remnants
+    partitions = {
+        layer_idx: louvain(remnant).to_node_community_map()
+        for layer_idx, remnant in remnants.items()
+    }
+    ### * Need to adjust for cdlib output returning a list of community assignments!
+    for layer_idx, mapping in partitions.items():
+        partitions[layer_idx] = mapping[0]
+
+    # Calculate community strength, mu
+    p_ins = {layer_idx: 0 for layer_idx in remnants}
+    p_outs = {layer_idx: 0 for layer_idx in remnants}
+
+    # Calculate probability of internal and external edges to groups
+    for idx, remnant in remnants.items():
+        edge_incident_communities = \
+            np.array([
+                [partitions[layer_idx][edge[0]] for edge in remnant.edges],
+                [partitions[layer_idx][edge[1]] for edge in remnant.edges]
+            ])
+
+        p_ins[layer_idx] = \
+            np.count_nonzero(np.array(edge_incident_communities[0] == edge_incident_communities[1], dtype=int))
+        p_outs[layer_idx] = len(remnant.edges)
+
+    # Calculate communtiy strength
+    try:
+        mu = sum(p_ins.values()) / (sum(p_ins.values()) + sum(p_outs.values()))
+    except ZeroDivisionError:
+        mu = 0.5
+
+    return partitions, mu
 
 
 # ----------------- Drivers ----------------
-def reconstruct_from_observations(aggregate, observations=None,
-        use_community_detection=False, soft_classifications=False):
-    pass
+def reconstruct_from_observations(aggregate, observations: dict = None,
+        use_community_detection: bool = False, soft_classifications: bool = False):
+    # Book-keeping
+    reconstruction_mapping = {}
 
+    # Form remnants
+    # * Used to inform likelihood calculations
+    ## Form edgelists
+    remnants_ = {
+        layer_idx: set(aggregate) - set(observed_layer) for layer_idx, observed_layer in observations.values()
+    }
 
+    ## Get all nodes
+    ## * Avoids missing node indices!
+    remnant_nodes = set.union(*[list(chain(*rem)) for rem in remnants_])
 
-"""
-def partial_information_experiment(G,exp_type,iterations=10):
-    #G: [g1,g2]
-    #exp_type: 'd' or 'cd'
-    A,partial_info=set(G[0].edges()).symmetric_difference(set(G[1].edges())),np.linspace(0,0.95,10)
-    GE1,GE2=set(G[0].edges),set(G[1].edges)
+    ## Form nx.Graph objects and track degree sequences
+    remnants = {}
+    degrees = {}
+    for layer_idx in remnants_:
+        ### Initialize graph
+        graph_ = nx.Graph()
 
-    aucs = []
-    for info in partial_info:
-        aucs_ = []
-        for _ in range(iterations):
+        ### Add nodes (even if under XOR they may be isolated in some layer)
+        graph_.add_nodes_from(remnant_nodes)
 
-            kE=set(random.sample(A,int(len(A)*info)))
+        ### Add remnant edges
+        graph_.add_edges_from(remnants_[layer_idx])
 
-            kE1=kE.intersection(GE1)
-            kE2=kE.intersection(GE2)
+        ### Update graph object
+        remnants[layer_idx] = graph_
 
-            k1 = A.difference(kE2)
-            k2 = A.difference(kE1)
+        ### Track degree sequences for likelihood calculations
+        degrees[layer_idx] = graph_.degree()
 
-            nodes_k1 = [edge[0] for edge in k1] + [edge[1] for edge in k1]
-            nodes_k2 = [edge[0] for edge in k2] + [edge[1] for edge in k2]
-            nodes_common = set(nodes_k1) | set(nodes_k2)
+    # Start reconstruction body
+    ## Calculate communities, if applicable
+    if use_community_detection:
+        partitions, mu = get_partitions(remnants)
+    else:
+        partitions, mu = None, 0.5  # Dummy values for likelihood if communities not utilized
 
-            G1_remnant,G2_remnant=nx.Graph(),nx.Graph()
-            G1_remnant.add_nodes_from(nodes_common)
-            G2_remnant.add_nodes_from(nodes_common)
-            G1_remnant.add_edges_from(k1)
-            G2_remnant.add_edges_from(k2)
+    ## Loop over unclassified edges
+    unknown_aggregate_edges = \
+        set(aggregate).difference(
+            set.union(*[edges for edges in observations.values()])
+        )
+    for edge in unknown_aggregate_edges:
+        ### Calculate likelihood of edge belonging to each layer
+        likelihoods = calculate_edge_likelihoods(edge, degrees, mu, partitions)
 
-            d1,d2=G1_remnant.degree(),G2_remnant.degree()
+        ### Hard classification - take most likely layer
+        reconstruction_mapping[edge] = max(likelihoods, key=likelihoods.get)
 
-            if exp_type=='cd':
-                partition1 = louvain(G1_remnant).to_node_community_map()
-                partition1 = {key: value[0] for key, value in partition1.items()}
-                partition2 = louvain(G2_remnant).to_node_community_map()
-                partition2 = {key: value[0] for key, value in partition2.items()}
-
-                pin1,pout1=0,0
-                for edge in G1_remnant.edges():
-                    if partition1[edge[0]]==partition1[edge[1]]:
-                        pin1+=1
-                    else:
-                        pout1+=1
-                pin2,pout2=0,0
-                for edge in G2_remnant.edges():
-                    if partition2[edge[0]]==partition2[edge[1]]:
-                        pin2+=1
-                    else:
-                        pout2+=1
-                mu=0.5
-                if (pin1+pin2+pout1+pout2)>0:
-                    mu=(pin1+pin2)/(pin1+pin2+pout1+pout2)
-
-            classification=[]
-            guesses = 0
-            ground_truth=[]
-            for edge in A.difference(kE):
-                if edge in GE1:
-                    ground_truth.append(0)
-                elif edge in GE2:
-                    ground_truth.append(1)
-                if exp_type=='d':
-
-                    p1=(d1[edge[0]]*d1[edge[1]]/(d1[edge[0]]*d1[edge[1]]+d2[edge[0]]*d2[edge[1]]))
-                    p2=(d2[edge[0]]*d2[edge[1]]/(d1[edge[0]]*d1[edge[1]]+d2[edge[0]]*d2[edge[1]]))
-                    if p1>p2:
-                        classification.append(0)
-                    elif p2>p1:
-                        classification.append(1)
-                    else:
-                        classification.append(random.randint(0,1))
-
-                elif exp_type=='cd':
-
-                    if partition1[edge[0]]==partition1[edge[1]]:
-                        p1=(d1[edge[0]]*d1[edge[1]]/(d1[edge[0]]*d1[edge[1]]+d2[edge[0]]*d2[edge[1]]))*mu
-                    else:
-                        p1=(d1[edge[0]]*d1[edge[1]]/(d1[edge[0]]*d1[edge[1]]+d2[edge[0]]*d2[edge[1]]))*(1-mu)
-                    if partition2[edge[0]]==partition2[edge[1]]:
-                        p2=(d2[edge[0]]*d2[edge[1]]/(d1[edge[0]]*d1[edge[1]]+d2[edge[0]]*d2[edge[1]]))*mu
-                    else:
-                        p2=(d2[edge[0]]*d2[edge[1]]/(d1[edge[0]]*d1[edge[1]]+d2[edge[0]]*d2[edge[1]]))*(1-mu)
-
-                    if p1>p2:
-                        classification.append(0)
-                    elif p2>p1:
-                        classification.append(1)
-                    else:
-                        classification.append(random.randint(0,1))
-
-            aucs_.append(metrics.accuracy_score(ground_truth, classification))
-        aucs.append(np.mean(aucs_))
-
-    return aucs
-"""
+    return reconstruction_mapping
